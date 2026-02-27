958f870885214ffb6d5b4936e15a66ea01a237f6
David Obazuaye <davidbaz@live.com>
2026-02-27 19:13:50 +0000

    Minor fix for social links to avoid returning null

--- src/main/java/com/baz/searchapi/model/dto/ClientResponse.java
+++ src/main/java/com/baz/searchapi/model/dto/ClientResponse.java
@@ -1,8 +1,11 @@
 package com.baz.searchapi.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ClientResponse(
         UUID id,
         String firstName,
@@ -11,4 +14,7 @@ public record ClientResponse(
         String description,
         List<String> socialLinks
 ) {
+    public ClientResponse {
+        socialLinks = socialLinks != null ? List.copyOf(socialLinks) : List.of();
+    }
 }
